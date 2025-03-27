392cd73879f33169f5cb1f31a273218d6b4cbbe6
Brandon Saint-John <bsaintjo@ucsc.edu>
2025-03-27 04:28:42 +0000

    Add ioctl

--- kcounter.rs
+++ kcounter.rs
@@ -1,3 +1,6 @@
+/// This code implements a simple Rust Linux module that counts the number of
+/// times it is opened.
+
 use core::{
     pin::Pin,
     sync::atomic::{AtomicU32, Ordering},
@@ -5,11 +8,13 @@ use core::{
 use kernel::{
     alloc::KBox,
     c_str,
+    device::Device,
     error::{Error, Result},
     fs::file::File,
     ioctl::{_IOC_SIZE, _IOR},
     miscdevice::{MiscDevice, MiscDeviceOptions, MiscDeviceRegistration},
     prelude::*,
+    types::ARef,
     uaccess::{UserSlice, UserSliceWriter},
 };
 
@@ -44,13 +49,14 @@ impl kernel::InPlaceModule for RustKCounterModule {
 #[pin_data(PinnedDrop)]
 struct KCounterDevice {
     counter: AtomicU32,
-    // dev: ARef<Device>,
+    dev: ARef<Device>,
 }
 
 impl KCounterDevice {
-    fn new() -> impl PinInit<Self, Error> {
+    fn new(dev: ARef<Device>) -> impl PinInit<Self, Error> {
         try_pin_init!( Self {
-            counter: AtomicU32::default()
+            counter: AtomicU32::default(),
+            dev: dev
         }? Error)
     }
 
@@ -58,7 +64,7 @@ impl KCounterDevice {
         let n = self.counter.fetch_add(1, Ordering::Relaxed);
         uslice
             .write(&n)
-            .inspect_err(|_| log::error!("User slice full"))?;
+            .inspect_err(|_| pr_err!("User slice full"))?;
         Ok(0)
     }
 }
@@ -69,22 +75,22 @@ const RESPOND: u32 = _IOR::<i32>('|' as u32, 0x81);
 impl MiscDevice for KCounterDevice {
     type Ptr = Pin<KBox<Self>>;
 
-    fn open(_file: &File, _misc: &MiscDeviceRegistration<Self>) -> Result<Self::Ptr> {
-        // let dev = ARef::from(misc.device());
+    fn open(_file: &File, misc: &MiscDeviceRegistration<Self>) -> Result<Self::Ptr> {
+        let dev = ARef::from(misc.device());
         // let inner = try_pin_init!(KCounterDevice {
         //     counter: AtomicU32::default(),
         //     // dev: dev,
         // });
-        KBox::try_pin_init(Self::new(), GFP_KERNEL)
+        KBox::try_pin_init(Self::new(dev), GFP_KERNEL)
     }
 
-    fn ioctl(self: Pin<&KCounterDevice>, _file: &File, cmd: u32, arg: usize) -> Result<isize> {
-        dev_info!(self.dev, "IOCTLing Rust KCounter\n");
+    fn ioctl(me: Pin<&KCounterDevice>, _file: &File, cmd: u32, arg: usize) -> Result<isize> {
+        dev_info!(me.dev, "IOCTLing Rust KCounter\n");
 
         let size = _IOC_SIZE(cmd);
 
         match cmd {
-            READ_VALUE => self.respond(UserSlice::new(arg, size).writer())?,
+            RESPOND => me.respond(UserSlice::new(arg, size).writer())?,
             // RUST_MISC_DEV_SET_VALUE => me.set_value(UserSlice::new(arg, size).reader())?,
             // RUST_MISC_DEV_HELLO => me.hello()?,
             _ => {
